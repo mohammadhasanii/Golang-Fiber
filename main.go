32dd6a756884a95aa73f0fc18fe1796087931d06
Mohammad Hasani <77454712+mohammadmahdihasani@users.noreply.github.com>
2023-04-08 14:25:57 +0330

    Update main.go

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
-
-func bootstrap(){
+func bootstrap() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, Fiber (Framework Golang)")
-	  })
-    
-	  app.Listen(":3000")
-    
+	})
+
+	app.Listen(":3000")
+
 }
 
-
-func main(){
+func main() {
 
 	bootstrap()
 }
